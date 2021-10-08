076d5447b1fe5a58d86383808557a08871f33fc6
hossein20s <hossein.akhlaghpour@gmail.com>
2021-10-08 15:08:51 -0700

    success

--- zig_zag.py
+++ zig_zag.py
@@ -3,36 +3,32 @@ from unittest import TestCase, main
 
 def trim(list):
     result = ""
-    index = 0
-    while index < len(list):
-        if list[index] is None:
-            list.remove(list[index])
-        else:
-            index += 1
-    print(list)
-    return result.join(list)
+    for i in range(len(list)):
+        result += ''.join(list[i])
+    return result
+
     
         
 def xxx(start, result, s, numRows):
     n = len(s)
-    last_filled_index = 0
     for i in range(numRows):
-        if start == len(s):
+        if start >= n:
             return None
-        last_filled_index = start + i * n
-        result[last_filled_index] = s[start]
+        if i == len(result):
+            result.append([])
+        result[i].append(s[start])
         start += 1
-    for i in range(numRows-2):
-        if start == len(s):
+    for i in range(numRows-2,0,-1):
+        if start >= n:
             return None
-        j = i + 1
-        result[last_filled_index- n*j+j] = s[start]
+        # result[i].extend([None]*i)
+        result[i].append(s[start])
         start += 1
     return start
     
 def zig_zag(s, numRows):
 
-    result = [None] * numRows * len(s)
+    result = []
     start = 0
     while start is not None:
         start = xxx(start, result, s, numRows)
@@ -45,6 +41,7 @@ class TestSolution(TestCase):
         self.assertEqual(zig_zag( s = "PAYPALISHIRING", numRows = 3), "PAHNAPLSIIGYIR")
         self.assertEqual(zig_zag( s = "PAYPALISHIRING", numRows = 4), "PINALSIGYAHRPI")
         self.assertEqual(zig_zag( s = "A", numRows = 1), "A")
+        self.assertEqual(zig_zag( s = "nwwzcdsxrhcolbbdddhmvuhvgaymcpyrlxq", numRows = 15), " ncwmpwyyzarcgldvxshqxurvhmchodldbdb")
 
 
 if __name__ == "__main__":
